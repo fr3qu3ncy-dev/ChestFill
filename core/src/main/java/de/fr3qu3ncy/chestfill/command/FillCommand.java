524e9e4b23a4e39d5c518824b7760c5e4c4d6665
fr3qu3ncy <kilicbatuhan1205@gmail.com>
2021-12-23 12:03:42 +0100

    Updated to 1.1

--- core/src/main/java/de/fr3qu3ncy/chestfill/command/FillCommand.java
+++ core/src/main/java/de/fr3qu3ncy/chestfill/command/FillCommand.java
@@ -6,10 +6,14 @@ import de.fr3qu3ncy.chestfill.VersionProvider;
 import de.fr3qu3ncy.chestfill.lang.Lang;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FillCommand extends BukkitCommand {
 
@@ -56,9 +60,20 @@ public class FillCommand extends BukkitCommand {
             return;
         }
 
+
         //Get the item in player's main hand
         ItemStack heldItem = ChestFill.getVersionProvider().getItemInHand(player).clone();
+
+        //Check if player specified an amount
         int stackSize = heldItem.getMaxStackSize();
+        if (args.length >= 1) {
+            try {
+                stackSize = Integer.parseInt(args[0]);
+                if (stackSize > 64) stackSize = 64;
+                if (stackSize < 0) stackSize = 1;
+            } catch (NumberFormatException ignored) {}
+        }
+
         heldItem.setAmount(stackSize);
 
         //Check if player is holding an item
@@ -72,9 +87,11 @@ public class FillCommand extends BukkitCommand {
 
         //Populate the inventory
         blockInv.clear();
+        List<ItemStack> items = new ArrayList<>();
         for (int i = 0 ; i < blockInv.getSize() ; i++) {
-            blockInv.addItem(heldItem);
+            items.add(heldItem);
         }
+        blockInv.setContents(items.toArray(new ItemStack[0]));
 
         player.sendMessage(lang.getFillSuccessful());
     }
